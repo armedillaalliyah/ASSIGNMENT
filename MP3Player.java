6e6b284852daf477484636f5e6b1e5bbea2b2105
Alliyah Samantha Armedilla <156239716+armedillaalliyah@users.noreply.github.com>
2024-04-24 23:59:16 +0800

    Add files via upload

---
+++ MP3Player.java
@@ -0,0 +1,111 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+
+interface Playable {
+    void load(String fileName) throws UnsupportedAudioFileException, IOException, LineUnavailableException;
+    void play();
+    void stop();
+    void reset();
+    void close();
+}
+
+class MusicPlayer implements Playable {
+    private Clip clip;
+    private String trackName;
+
+    public MusicPlayer(String trackName) {
+        this.trackName = trackName;
+    }
+
+    @Override
+    public void load(String fileName) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File(fileName);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        clip = AudioSystem.getClip();
+        clip.open(audioStream);
+    }
+
+    @Override
+    public void play() {
+        clip.start();
+        System.out.println("Playing: " + trackName);
+    }
+
+    @Override
+    public void stop() {
+        clip.stop();
+        System.out.println("Stopped: " + trackName);
+    }
+
+    @Override
+    public void reset() {
+        clip.setMicrosecondPosition(0);
+        System.out.println("Reset: " + trackName);
+    }
+
+    @Override
+    public void close() {
+        clip.close();
+        System.out.println("Closed: " + trackName);
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+}
+
+public class MP3Player {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Scanner scanner = new Scanner(System.in);
+        Playable[] tracks = {
+            new MusicPlayer("Track 1"),
+            new MusicPlayer("Track 2"),
+            new MusicPlayer("Track 3")
+        };
+
+        int choice;
+
+        System.out.println("Select a track to play:");
+        for (int i = 0; i < tracks.length; i++) {
+            System.out.println((i + 1) + ". " + ((MusicPlayer) tracks[i]).getTrackName());
+        }
+        System.out.print("Enter your choice: ");
+        choice = scanner.nextInt();
+        choice--;
+
+        String[] fileNames = {"iloveyou.wav", "joy.wav", "end.wav"};
+        tracks[choice].load(fileNames[choice]);
+
+        String response = "";
+
+        try {
+            while (!response.equals("Q")) {
+                System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+                System.out.print("Enter your choice: ");
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P":
+                        tracks[choice].play();
+                        break;
+                    case "S":
+                        tracks[choice].stop();
+                        break;
+                    case "R":
+                        tracks[choice].reset();
+                        break;
+                    case "Q":
+                        tracks[choice].close();
+                        break;
+                    default:
+                        System.out.println("Please input a valid response");
+                }
+            }
+        } finally {
+            scanner.close();
+        }
+        System.out.println("Thank you for listening!!");
+    }
+}
